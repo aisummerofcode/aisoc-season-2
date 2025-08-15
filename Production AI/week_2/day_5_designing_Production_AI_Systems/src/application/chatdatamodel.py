3f5fbffd9a6470e55e5c3b0839ea60dc73c3f951
ayo <ayo@clivealliance.com>
2025-08-15 13:00:34 +0100

    Remove duplicate folder
    Removed duplicate folder and renamed folder to day_5_sys_design

--- Production AI/week_2/day_5_designing_Production_AI_Systems/src/application/chatdatamodel.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Optional
-
-
-class ChatRequest(BaseModel):
-    message: str = Field("")
-    name: str = Field("")
-    location: Optional[str] = Field("lagos, Nigeria")
--- Production AI/week_2/day_5_designing_Production_AI_Systems/src/services/chatagent.py
+++
@@ -1,2 +0,0 @@
-def call_agent(chatRequest: ChatRequest):
-    pass
