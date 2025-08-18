73ed53cba37f72dac8af67eb4002581c2017b51f
Zion <pibzion@gmail.com>
2025-08-18 13:13:33 +0100

    revert main.py to previous version

--- Applied AI/week_3/day_4_rag_deepdive/main.py
+++ Applied AI/week_3/day_4_rag_deepdive/main.py
@@ -86,7 +86,7 @@ if __name__=="__main__":
 
     index = retrieve_embedding()
 
-    query = "What was my last question?"
+    query = "What is AI Summer of Code"
 
     print("\nStarting response generation...\n")
     start_time=time.time()
